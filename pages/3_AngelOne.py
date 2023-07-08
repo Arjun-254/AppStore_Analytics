f4e3eff581168d783df9b775077178317c89beaa
Arjun-254 <arjun.shah254@gmail.com>
2023-07-08 17:26:32 +0530

    cachetest

--- pages/3_AngelOne.py
+++ pages/3_AngelOne.py
@@ -14,39 +14,14 @@ import joblib
 import plotly.graph_objects as go
 from func import analyze_reviews
 
-# # Streamlit app to be full screen
-# # st.set_page_config(layout="wide")
-# st.markdown('<h1 style="font-size: 70px; color: #E3142D;"> Angel One </h1>',
-#             unsafe_allow_html=True)
-# st.markdown('<h1 style="font-size: 70px; color: #9347ED;">Customer Review Analytics</h1>',
-#             unsafe_allow_html=True)
-
-# stop_words = {'angel', 'one', 'please',
-#               'able', 'bank', 'app', 'good', 'nice', 'best'}
-# analyze_reviews('ReviewsA.csv', stop_words)
-
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from google_play_scraper import app, Sort, reviews_all
-import pandas as pd
-import numpy as np
-from datetime import date, datetime, timedelta
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk import ngrams
-from collections import Counter
-import streamlit as st
-import altair as alt
-import joblib
-import plotly.graph_objects as go
-
-# Streamlit app to be full screen
-# st.set_page_config(layout="wide")
-
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+
+st.markdown('<h1 style="font-size: 70px; color: #E3142D;"> Angel One </h1>',
+            unsafe_allow_html=True)
+st.markdown('<h1 style="font-size: 70px; color: #9347ED;">Customer Review Analytics</h1>',
+            unsafe_allow_html=True)
 
 
 # Set the cache expiry time to 24 hours
